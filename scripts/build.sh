dfc41ab08a393da5a12fc4e1d4f505f39b8b4241
Alexandre BIQUE <bique.alexandre@gmail.com>
2022-01-28 23:28:58 +0100

    update linux deps

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,13 +10,13 @@ if [[ "$1" != "" ]]; then
 fi
 
 if [[ $(uname) = Linux ]] ; then
-  QT_FEATURES=",xcb"
+  QT_FEATURES=",xcb,xcb-xlib,xkb,xkbcommon-x11,xlib,xrender"
 elif [[ $(uname) = Darwin ]] ; then
   QT_FEATURES=""
 fi
 
 vcpkg/vcpkg --overlay-triplets=vcpkg-overlay/triplets $vcpkg_triplet install \
-  rtmidi rtaudio "qtbase[core,png,widgets,doubleconversion,concurrent,appstore-compliant${QT_FEATURES}]"
+  rtmidi rtaudio "qtbase[core,png,widgets,doubleconversion,concurrent,appstore-compliant,freetype,harfbuzz,${QT_FEATURES}]"
 
 cmake --preset ninja-vcpkg $cmake_triplet
 cmake --build --preset ninja-vcpkg-release --target clap-host
