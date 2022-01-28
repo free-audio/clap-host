08115081cbae44e859bc473b87b54daf6a63cd7e
Alexandre BIQUE <bique.alexandre@gmail.com>
2022-01-28 23:21:30 +0100

    Rework build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,19 +5,19 @@ if [[ ! -x vcpkg/vcpkg ]] ; then
 fi
 
 if [[ "$1" != "" ]]; then
-  triplet="--triplet $1"
+  vcpkg_triplet="--triplet $1"
+  cmake_triplet="-DVCPKG_TARGET_TRIPLET=$1"
 fi
 
 if [[ $(uname) = Linux ]] ; then
-  QT_FEATURES=",xcb,fontconfig,xrender,xkb"
+  QT_FEATURES=",xcb"
 elif [[ $(uname) = Darwin ]] ; then
   QT_FEATURES=""
 fi
 
-vcpkg/vcpkg --overlay-triplets=vcpkg-overlay/triplets $triplet install \
+vcpkg/vcpkg --overlay-triplets=vcpkg-overlay/triplets $vcpkg_triplet install \
   rtmidi rtaudio "qtbase[core,png,widgets,doubleconversion,concurrent,appstore-compliant${QT_FEATURES}]"
 
-
-cmake --preset ninja-vcpkg
+cmake --preset ninja-vcpkg $cmake_triplet
 cmake --build --preset ninja-vcpkg-release --target clap-host
 tar -cvJf clap-host.tar.xz --strip-components=4 builds/ninja-vcpkg/host/Release/clap-host
