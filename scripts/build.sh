7adaaf6618af3f9f0c740274a2765e4a7b3d89ec
Alexandre BIQUE <bique.alexandre@gmail.com>
2022-01-28 19:28:32 +0100

    Try this

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,15 +1,15 @@
 #! /bin/bash
 
-cd vcpkg
-if [[ ! -x vcpkg ]] ; then
-  ./bootstrap-vcpkg.sh
+
+if [[ ! -x vcpkg/vcpkg ]] ; then
+  vcpkg/bootstrap-vcpkg.sh
 fi
 
 # qtbase[core,widgets,doubleconversion,concurrent]
-./vcpkg --overlay-triplets=$PWD/../vcpkg-overlay/triplets install \
+vcpkg/vcpkg --overlay-triplets=$PWD/../vcpkg-overlay/triplets install \
   rtmidi rtaudio
 
-cd ..
+
 cmake --preset ninja-vcpkg
 cmake --build --preset ninja-vcpkg-release --target clap-host
 tar -cvJf clap-host.tar.xz --strip-components=4 builds/ninja-vcpkg/host/Release/clap-host
