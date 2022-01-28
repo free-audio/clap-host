a83b3f7051b074a33ee7586c0376e792eb52988f
Alexandre BIQUE <bique.alexandre@gmail.com>
2022-01-28 22:45:05 +0100

    Universal binaries are problematic with vcpkg due to simd

--- vcpkg-overlay/triplets/arm64-osx.cmake
+++ vcpkg-overlay/triplets/arm64-osx.cmake
@@ -5,7 +5,7 @@ set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
 
 set(VCPKG_CMAKE_SYSTEM_NAME Darwin)
-set(VCPKG_OSX_ARCHITECTURES x86_64;arm64)
+set(VCPKG_OSX_ARCHITECTURES arm64)
 
 set(VCPKG_C_FLAGS_RELEASE "-Os -fomit-frame-pointer")
 set(VCPKG_CXX_FLAGS_RELEASE "-Os -fomit-frame-pointer")
