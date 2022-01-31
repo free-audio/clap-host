7a74c36f06ba51c7d12a4915a0a6de2a4cd5d534
Alexandre BIQUE <bique.alexandre@gmail.com>
2022-01-31 10:36:56 +0100

    Don't use native menu as they are bad especially on mac

--- host/main.cc
+++ host/main.cc
@@ -5,6 +5,7 @@
 #include "application.hh"
 
 int main(int argc, char *argv[]) {
+   QApplication::setAttribute(Qt::AA_DontUseNativeMenuBar);
    Application app(argc, argv);
    return app.exec();
 }
