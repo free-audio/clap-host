73d6e0ba2bf9079f76ed37da0e10fde48c6b49d9
Alexandre BIQUE <bique.alexandre@gmail.com>
2022-02-03 00:50:02 +0100

    get rid of .ps1 script

--- scripts/build.ps1
+++
@@ -1,7 +0,0 @@
-cd vcpkg
-bootstrap-vcpkg.bat
-
-vcpkg --overlay-triplets=$PWD/../vcpkg-overlay/triplets install --recurse rtmidi rtaudio qtbase[core,widgets,doubleconversion,concurrent]
-cd ..
-cmake --preset ninja-vcpkg
-cmake --build --preset ninja-vcpkg-release --target clap-host
