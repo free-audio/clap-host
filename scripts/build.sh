492b29c8f6eb6438fd4777405ebb26bc7776714a
Alexandre BIQUE <bique.alexandre@gmail.com>
2022-02-03 00:37:55 +0100

    Update the build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,24 +4,32 @@ if [[ ! -x vcpkg/vcpkg ]] ; then
   vcpkg/bootstrap-vcpkg.sh
 fi
 
-if [[ "$1" != "" ]]; then
-  vcpkg_triplet="--triplet $1"
-  cmake_triplet="-DVCPKG_TARGET_TRIPLET=$1"
-fi
-
 if [[ $(uname) = Linux ]] ; then
   QT_FEATURES=",xcb,xcb-xlib,xkb,xkbcommon-x11,xlib,xrender,fontconfig,harfbuzz"
   cmake_preset="ninja-vcpkg"
+  triplet=$(uname -m)-linux
 elif [[ $(uname) = Darwin ]] ; then
   QT_FEATURES=",harfbuzz"
   cmake_preset="ninja-vcpkg"
+  triplet=$(uname -m)-osx
 else
   QT_FEATURES=""
   cmake_preset="vs-vcpkg"
+  triplet=$(uname -m)-windows
 fi
 
+if [[ "$1" != "" ]]; then
+  triplet="$1"
+fi
+
+vcpkg_triplet="--triplet ${triplet}-clap-host --host-triplet ${triplet}-clap-host"
+cmake_triplet="-DVCPKG_TARGET_TRIPLET=${triplet}-clap-host -DCMAKE_VCPKG_HOST_TRIPLET=${triplet}-clap-host"
+
 vcpkg/vcpkg --overlay-triplets=vcpkg-overlay/triplets $vcpkg_triplet install --recurse \
-  rtmidi rtaudio "qtbase[core,png,widgets,doubleconversion,concurrent,appstore-compliant,freetype${QT_FEATURES}]"
+  rtmidi rtaudio "core,png,widgets,doubleconversion,concurrent,appstore-compliant,freetype${QT_FEATURES}]"
+
+# save space
+rm -rf vcpkg/buildtrees
 
 cmake --preset $cmake_preset $cmake_triplet
-cmake --build --preset $cmake_preset --config Release --target clap-host
+cmake --build --preset $cmake_preset --config Release --target clap-plugins
