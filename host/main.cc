eba898bf54090d3bfd70933920fe7dbdf2d58f90
Alexandre BIQUE <bique.alexandre@gmail.com>
2022-02-25 16:41:02 +0100

    Make sure to use x11 on linux for now

--- host/main.cc
+++ host/main.cc
@@ -1,10 +1,15 @@
-﻿#include <cstdio>
+﻿#include <cstdlib>
 
 #include <QApplication>
 
 #include "application.hh"
 
 int main(int argc, char *argv[]) {
+
+#ifdef Q_OS_LINUX
+   ::setenv("QT_QPA_PLATFORM", "xcb", 1);
+#endif
+
    QApplication::setAttribute(Qt::AA_DontUseNativeMenuBar);
    Application app(argc, argv);
    return app.exec();
