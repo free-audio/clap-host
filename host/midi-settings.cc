ccbd4e5913d3eb5e418fbc56eda1671f1237412e
Alexandre BIQUE <bique.alexandre@gmail.com>
2022-03-19 16:58:54 +0100

    Store the midi device api

--- host/midi-settings.cc
+++ host/midi-settings.cc
@@ -2,12 +2,14 @@
 
 #include "midi-settings.hh"
 
+static const char API_KEY[] = "MIDI/API";
 static const char DEVICE_NAME_KEY[] = "Midi/DeviceName";
 static const char DEVICE_INDEX_KEY[] = "Midi/DeviceIndex";
 
 MidiSettings::MidiSettings() {}
 
 void MidiSettings::load(QSettings &settings) {
+   _deviceReference._api = settings.value(API_KEY).toString();
    _deviceReference._name = settings.value(DEVICE_NAME_KEY).toString();
    _deviceReference._index = settings.value(DEVICE_INDEX_KEY).toInt();
 }
@@ -15,4 +17,5 @@ void MidiSettings::load(QSettings &settings) {
 void MidiSettings::save(QSettings &settings) const {
    settings.setValue(DEVICE_NAME_KEY, _deviceReference._name);
    settings.setValue(DEVICE_INDEX_KEY, _deviceReference._index);
+   settings.setValue(API_KEY, _deviceReference._api);
 }
