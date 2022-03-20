200d01b5a224e658f919356f387bbcb7b3067aa3
Alexandre BIQUE <bique.alexandre@gmail.com>
2022-03-20 09:57:54 +0100

    Add clap version to clap host in about dialog

--- host/about-dialog.cc
+++ host/about-dialog.cc
@@ -1,9 +1,19 @@
+#include <sstream>
+
+#include <clap/version.h>
+
 #include "about-dialog.hh"
 
 AboutDialog::AboutDialog(QObject *parent) {
-   setTextFormat(Qt::TextFormat::MarkdownText);
-   setText(R"(# clap-host
+   std::ostringstream s;
+
+   s << R"(# clap-host
 Example host for [**CLAP**](https://github.com/free-audio/clap).
 
-Sources on [Github](https://github.com/free-audio/clap-host).)");
+Sources on [Github](https://github.com/free-audio/clap-host).
+
+clap-)" << CLAP_VERSION_MAJOR << "." << CLAP_VERSION_MINOR << "." << CLAP_VERSION_REVISION;
+
+   setTextFormat(Qt::TextFormat::MarkdownText);
+   setText(QString::fromStdString(s.str()));
 }
