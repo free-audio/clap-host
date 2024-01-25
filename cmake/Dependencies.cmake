297fba98ba9758f812876d59b7cf77d7cdcf626e
Yuri Witte <witte@tuta.io>
2024-01-25 15:40:21 -0300

    don't depend on submodules for clap and clap-helpers

---
+++ cmake/Dependencies.cmake
@@ -0,0 +1,22 @@
+include(FetchContent)
+
+FetchContent_Declare(
+    clap
+    GIT_REPOSITORY https://github.com/free-audio/clap.git
+    GIT_TAG main
+    SYSTEM
+    # 'FIND_PACKAGE_ARGS' will skip download if
+    # the target is already available in the system
+    FIND_PACKAGE_ARGS NAMES clap
+)
+
+FetchContent_Declare(
+    clap-helpers
+    GIT_REPOSITORY https://github.com/free-audio/clap-helpers.git
+    GIT_TAG main
+    SYSTEM
+    FIND_PACKAGE_ARGS NAMES clap-helpers
+)
+
+
+FetchContent_MakeAvailable(clap clap-helpers)
